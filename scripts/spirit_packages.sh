bc9e4b15bbd1b3ac2a69d9f056b3ccf56d5e1777
Genie Jhang <geniejhang@majimak.com>
2014-11-25 18:12:30 +0900

    - Installation scripts are added -- CLHEP 2.2.0.4 -- RAVE 0.6.21

---
+++ scripts/spirit_packages.sh
@@ -0,0 +1,5 @@
+export RAVE_LOCATION="http://www.hepforge.org/archive/rave"
+export RAVE_VERSION=rave-0.6.21
+
+export CLHEP_LOCATION="http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles"
+export CLHEP_VERSION=2.2.0.4
